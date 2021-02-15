f90c51b21d45025dfd39a3a166b822ba4991d29d
Carlos López <contacto@carloslopezmari.com>
2021-02-15 22:09:36 +0100

    Adding it

---
+++ src/main/java/com/carloslopez/koukiotest/graphql/resolvers/Query.java
@@ -0,0 +1,18 @@
+package com.carloslopez.koukiotest.graphql.resolvers;
+
+import com.carloslopez.koukiotest.controllers.PostController;
+import com.carloslopez.koukiotest.entities.Post;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+
+    @Autowired
+    private PostController postController;
+
+    public Iterable<Post> getLastPosts(int page, int size) {
+        return postController.getLastPosts(page, size);
+    }
+}
